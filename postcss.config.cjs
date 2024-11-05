be4e4289355670e014b15d56327dc8cd4bf2c1ca
David Rios <david@davidrios.co>
2024-11-04 21:11:22 -0500

    ♻️ Refactoring

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,13 +1,7 @@
-module.exports = (cfg) => {
-
-  const devMode = (cfg.env === "development");
-
-  return {
-    map : devMode ? "inline" : null,
-    plugins: [
-      require('tailwindcss')(),
-      require('autoprefixer')(),
-      devMode ? null : require('cssnano')() ,
-    ],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
   }
-};
+}
